b4a03254aefbdff15ded076e8c4645e819060eae
Leo-Green93 <139697327+Leo-Green93@users.noreply.github.com>
2023-12-25 12:48:53 -0500

    Store all components on CSS in js Create document.querySelector for all buttons, current and previous screens

---
+++ main.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function(){
+    //Store all components on HTML in JS
+    let operator = '';
+    let previousValue = '';
+    let currentValue = '';
+    
+    let clear = document.querySelector('.clear');
+    let equal = document.querySelector(".equal");
+    let decimal = document.querySelector('.decimal');
+
+    let numbers = documeent.querySelectorAll(".number");
+    let operators = document.querySelectorAll(".operator");
+
+    let previousScreen = document.querySelector(".previous");
+    let currentScreen = document.querySelector(".current");
+
+})
